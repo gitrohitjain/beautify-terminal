29fac94ce1a48e317a4016bb191ffaef3e1aa506
Rohit Jain <49694329+gitrohitjain@users.noreply.github.com>
2023-01-25 12:34:01 +0800

    Add files via upload

---
+++ beautify.sh
@@ -0,0 +1,9 @@
+apt-get install zsh
+cd ~ && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cd ~ && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/^ZSH_THEME\=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/^plugins\=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+chsh -s $(which zsh)
+echo "Installation Complete. Close terminal and open a ZSH terminal"
